e09988cec0e98ef16b9da45b4ab3801f19507bfc
Naratip Udomsilp <naratip.ud@gmail.com>
2016-09-19 21:26:36 +0700

    Modified Initial app class

--- app/app.ts
+++ app/app.ts
@@ -1,27 +1,26 @@
-import { module } from 'angular';
-
-export let app: angular.IModule = module('app', [
+export let app: angular.IModule = angular.module('app', [
     'ionic',
 ]);
 
-export class RunApp {
+export class InitApp {
     constructor($ionicPlatform: ionic.platform.IonicPlatformService, $window: angular.IWindowService) {
+        // Setup ionic platform.
         $ionicPlatform.ready(() => {
-            if (window.cordova && window.cordova.plugins.Keyboard) {
+            if ($window.cordova && window.cordova.plugins.Keyboard) {
                 // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
                 // for form inputs)
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                $window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
                 // Don't remove this line unless you know what you are doing. It stops the viewport
                 // from snapping when text inputs are focused. Ionic handles this internally for
                 // a much nicer keyboard experience.
-                cordova.plugins.Keyboard.disableScroll(true);
+                $window.cordova.plugins.Keyboard.disableScroll(true);
             }
-            if (window.StatusBar) {
+            if ($window.StatusBar) {
                 StatusBar.styleDefault();
             }
         });
     }
 }
 
-app.run(RunApp);
+app.run(InitApp);
