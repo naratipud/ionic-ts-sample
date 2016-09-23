e61fa03e99497797575541b360a063550c4856c6
Naratip Udomsilp <naratip.ud@gmail.com>
2016-09-23 19:31:31 +0700

    First commit

---
+++ app/pages/chats/chats.service.spec.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('Sample test', () => {
+
+    it('should behave...', () => {
+        expect(true).toEqual(true);
+    });
+    
+    it('should behave...', () => {
+        expect(false).toEqual(true);
+    });
+        
+});
